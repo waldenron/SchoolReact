a8910984ac6a93c40a1e2dda3774eab9e7edf2bf
Ronny walden <rony_1977@yahoo.com>
2023-09-28 18:38:57 +0300

    20230928_2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { ItemsList, Logo, Header } from "./Common"
+
+const instCode = "1";
 
 
 const instContactInfo = [
@@ -22,88 +25,52 @@ const instContactInfo = [
         link: "mailto:yesh@art-yeshiva.org.il",
     }
 ];
-const contacts = [
-    {
-        id: 6,
-        note: "",
-        lastUpdate: "2022-10-09",
-        firstName: "בני",
-        lastName: "פרל",
-        title: "הרב",
-        jobTitle: "ראש הישיבה",
-        email: "perel@art-yeshiva.org.il",
-        priority: 10,
-        fullName: "הרב בני פרל",
-        textSearch: "ראש הישיבה הרב בני פרל"
-    },
-    {
-        id: 7,
-        note: "",
-        lastUpdate: "2023-08-18",
-        firstName: "איתמר",
-        lastName: "רבינק",
-        title: "הרב",
-        jobTitle: "מנהל התיכון",
-        email: "rabinak@art-yeshiva.org.il",
-        priority: 20,
-        fullName: "הרב איתמר רבינק",
-        textSearch: "מנהל התיכון הרב איתמר רבינק"
-    },
-    {
-        id: 9,
-        note: "",
-        lastUpdate: "2023-08-18",
-        firstName: "ישי",
-        lastName: "לב",
-        title: "הרב",
-        jobTitle: "מנהל חטיבת הביניים ור\"מ ז´-2",
-        email: "lev@art-yeshiva.org.il",
-        priority: 30,
-        fullName: "הרב ישי לב",
-        textSearch: "מנהל חטיבת הביניים ור\"מ ז´-2 הרב ישי לב"
-    },
-    {
-        id: 8,
-        note: "",
-        lastUpdate: "2021-08-11",
-        firstName: "יפעת",
-        lastName: "כץ-אלוני",
-        title: "",
-        jobTitle: "מנהלנית אדמיניסטרטיבית",
-        email: "yfat@art-yeshiva.org.il",
-        priority: 40,
-        fullName: "יפעת כץ-אלוני",
-        textSearch: "מנהלנית אדמיניסטרטיבית יפעת כץ-אלוני"
-    },
-    {
-        id: 11,
-        note: "",
-        lastUpdate: "2023-08-18",
-        firstName: "שעיו בוקובזה",
-        lastName: "מיטל",
-        title: "",
-        jobTitle: "יועצת חינוכית - חט\"ב",
-        email: "shayo@art-yeshiva.org.il",
-        priority: 50,
-        fullName: "שעיו בוקובזה מיטל",
-        textSearch: "יועצת חינוכית - חט\"ב שעיו בוקובזה מיטל"
-    },
-    {
-        id: 10,
-        note: "",
-        lastUpdate: "2021-08-11",
-        firstName: "רועי",
-        lastName: "דולב",
-        title: "",
-        jobTitle: "יועץ חינוכי - חט\"ע",
-        email: "dolev@art-yeshiva.org.il",
-        priority: 60,
-        fullName: "רועי דולב",
-        textSearch: "יועץ חינוכי - חט\"ע רועי דולב"
-    }
+// const contacts = [
+//     {
+//         jobTitle: "ראש הישיבה",
+//         email: "perel@art-yeshiva.org.il",
+//         priority: 10,
+//         fullName: "הרב בני פרל",
+//         textSearch: "ראש הישיבה הרב בני פרל"
+//     },
+//     {
+//         jobTitle: "מנהל התיכון",
+//         email: "rabinak@art-yeshiva.org.il",
+//         priority: 20,
+//         fullName: "הרב איתמר רבינק",
+//         textSearch: "מנהל התיכון הרב איתמר רבינק"
+//     },
+//     {
+//         jobTitle: "מנהל חטיבת הביניים ור\"מ ז´-2",
+//         email: "lev@art-yeshiva.org.il",
+//         priority: 30,
+//         fullName: "הרב ישי לב",
+//         textSearch: "מנהל חטיבת הביניים ור\"מ ז´-2 הרב ישי לב"
+//     },
+//     {
+//         jobTitle: "מנהלנית אדמיניסטרטיבית",
+//         email: "yfat@art-yeshiva.org.il",
+//         priority: 40,
+//         fullName: "יפעת כץ-אלוני",
+//         textSearch: "מנהלנית אדמיניסטרטיבית יפעת כץ-אלוני"
+//     },
+//     {
+//         jobTitle: "יועצת חינוכית - חט\"ב",
+//         email: "shayo@art-yeshiva.org.il",
+//         priority: 50,
+//         fullName: "שעיו בוקובזה מיטל",
+//         textSearch: "יועצת חינוכית - חט\"ב שעיו בוקובזה מיטל"
+//     },
+//     {
+//        jobTitle: "יועץ חינוכי - חט\"ע",
+//         email: "dolev@art-yeshiva.org.il",
+//         priority: 60,
+//         fullName: "רועי דולב",
+//         textSearch: "יועץ חינוכי - חט\"ע רועי דולב"
+//     }
 
-    // ... Rest of the contacts follow the same structure
-];
+//     // ... Rest of the contacts follow the same structure
+// ];
 
 const toHtmlInstContactInfo = () => (
     <div id="PageHeaderMsg" className="d-flex-inline mx-auto">
@@ -135,6 +102,28 @@ const toHtmlElements = (contacts) => {
 
 
 const ContactPage = () => {
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        const url = '/api/Contacts';
+        //const url = 'https://api.f2a.info/api/Contacts';
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'InstCode': instCode   // Set your custom header here
+            }
+        }).then(response => response.json())
+            .then(data => {
+                const updatedData = data.map(contact => ({
+                    ...contact,
+                    textSearch: `${contact.jobTitle} ${contact.fullName}`
+                }));
+                updatedData.sort((a, b) => a.fullName - b.fullName);
+                setContacts(updatedData);
+            });
+    }, []);
+
     return (
         <div className="py-3 w-md-75 mx-auto">
             <Logo />
